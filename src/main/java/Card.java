71bd1e9b3d65f59d709b566469ebf59afeca5bad
doggytoaster <75501612+doggytoaster@users.noreply.github.com>
2023-10-21 18:55:34 -0500

    Update Card.java

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,1 +1,18 @@
+public class Card {
+    private String suite;
+    private int value;
 
+    // using for debugging / testing delete later
+    public String suiteValue(){
+        return this.suite;
+    }
+    // using for debugging / testing delete later
+    public int cardValue(){
+        return this.value;
+    }
+    // 2 argument constructor that creates a card with its value and suite
+    Card(String theSuite, int theValue){
+        this.suite = theSuite;
+        this.value = theValue;
+    }
+}
