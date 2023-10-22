96ffcdf14d45071de6184d973ebda86c232fbc86
wavygoten <bossmanzach007@gmail.com>
2023-10-21 22:55:09 -0500

    merged dealer and card

---
+++ src/main/java/Card.java
@@ -0,0 +1,10 @@
+public class Card {
+    public String suite;
+    public int value;
+
+    // 2 argument constructor that creates a card with its value and suite
+    Card(String theSuite, int theValue) {
+        this.suite = theSuite;
+        this.value = theValue;
+    }
+}
