c33f0eb95ee7c1a37c52c58046402ea50929e382
wavygoten <bossmanzach007@gmail.com>
2023-10-21 22:46:15 -0500

    added bacarratgamelogic class

---
+++ src/main/java/BaccaratGameLogic.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class BaccaratGameLogic {
+    public String whoWon(ArrayList<Card> hand1, ArrayList<Card> hand2) {
+        return "";
+    }
+
+    public int handTotal(ArrayList<Card> hand) {
+        return 0;
+    }
+
+    public boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard) {
+        return true;
+    }
+
+    public boolean evaluatePlayerDraw(ArrayList<Card> hand) {
+        return true;
+    }
+}
