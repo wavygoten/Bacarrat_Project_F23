ca39c0c7b6879357c1e43fc24a29f6bfb6de1b5e
doggytoaster <75501612+doggytoaster@users.noreply.github.com>
2023-10-23 01:19:03 -0500

    fix random stuff

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -1,23 +1,33 @@
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class BaccaratGame extends Application {
-	private ArrayList<Card> playerHand;
-	private ArrayList<Card> bankerHand;
-	private BaccaratDealer theDealer;
-	private double currentBet;
-	private double totalWinnings;
+	private ArrayList<Card> playerHand = new ArrayList<>();
+	private ArrayList<Card> bankerHand = new ArrayList<>();
+	private BaccaratDealer theDealer = new BaccaratDealer();
+	private double currentBalance = 0;
+	private double currentBet = 0;
+	private double totalWinnings = 0;
+	private BaccaratGameLogic logic = new BaccaratGameLogic();
 
 	public double evaluateWinnings() {
 
-		return 1;
+		double win = 0;
+
+		return win;
+
+		// do this bc ration multiplication
 	}
 
 	public static void main(String[] args) {
@@ -29,30 +39,72 @@ public class BaccaratGame extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("Adrian Lopez Homework 3");
-		BorderPane welcomePane = setupWelcomePage();
-		Scene scene = new Scene(welcomePane, 700, 700);
+		TextField bankerTotal = new TextField("Banker total");
+		TextField intBankerTotal = new TextField("$0");
+		TextField playerTotal = new TextField("Player total");
+		TextField intPlayerTotal = new TextField("$0");
+		TextField winnings = new TextField("Winnings");
+		TextField intWinnings = new TextField("$0");
+		TextField balance = new TextField("Balance");
+		TextField intBalance = new TextField("$0");
+		// Button Exit = new Button("Exit");
+		// Button Re = new Button("Fresh Start");
+		MenuBar mb = new MenuBar();
+		Menu options = new Menu("Options");
+		MenuItem exit = new MenuItem("Exit");
+		MenuItem re = new MenuItem("Restart");
+		// Menu re = new Menu("Restart");
+		exit.setOnAction(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		re.setOnAction(e -> freshRestart());
+		mb.getMenus().add(options);
+		options.getItems().addAll(exit, re);
+
+		// HBox topBar = new HBox(tb);
+		VBox leftSide = new VBox(bankerTotal, intBankerTotal, playerTotal, intPlayerTotal, winnings, intWinnings,
+				balance, intBalance);
+		HBox gamePane = new HBox();
+		HBox bottomBar = new HBox();
+		// totalMenu.setAlignment(Pos.CENTER);
+		BorderPane pane = new BorderPane(null, mb, gamePane, bottomBar, leftSide);
+
+		Scene scene = new Scene(pane, 700, 700);
 		scene.getStylesheets().add("styles.css");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 	}
 
-	private BorderPane setupWelcomePage() {
-		VBox mainMenu = new VBox();
-		TextField welcome = new TextField("Welcome to Baccarat");
-		welcome.getStyleClass().add("welcome");
-		TextField bet = new TextField("0$");
-		bet.getStyleClass().add("bet");
-		Button info = new Button("?");
-		info.getStyleClass().add("info");
-		Button playGame = new Button("Play Game");
-		mainMenu.getChildren().addAll(welcome, bet, playGame);
-		BorderPane pane = new BorderPane();
-		pane.getStyleClass().add("pane");
-		pane.setCenter(mainMenu);
-		pane.setBottom(info);
-		return pane;
+	private void play() {
+		this.theDealer.generateDeck();
+		this.bankerHand = this.theDealer.dealHand();
+		this.playerHand = this.theDealer.dealHand();
+		int playerTotal = logic.handTotal(playerHand);
+		int bankerTotal = logic.handTotal(bankerHand);
+
 	}
+
+	private void freshRestart() {
+
+		this.currentBalance = 100;
+		this.theDealer.generateDeck();
+		this.theDealer.shuffleDeck();
+		this.currentBet = 0;
+		this.totalWinnings = 0;
+		this.bankerHand.clear();
+		this.playerHand.clear();
+		this.play();
+		// do new play game
+
+	}
+	// private BorderPane setupWelcomePage() {
+
+	// BorderPane pane = new BorderPane();
+	// pane.getStyleClass().add("pane");
+
+	// return pane;
+	// }
 
 }
--- src/main/java/BaccaratGameLogic.java
+++ src/main/java/BaccaratGameLogic.java
@@ -1,19 +1,61 @@
 import java.util.ArrayList;
 
 public class BaccaratGameLogic {
+    private boolean playerDrew = false;
+
     public String whoWon(ArrayList<Card> hand1, ArrayList<Card> hand2) {
-        return "";
+        int playerTotal = handTotal(hand1);
+        int bankerTotal = handTotal(hand2);
+        if (playerTotal == bankerTotal) {
+            return "Draw.";
+        } else if (playerTotal > bankerTotal) {
+            return "Player.";
+        } else if (playerTotal == 0) {
+            return "Player.";
+        } else {
+            return "Banker.";
+        }
     }
 
     public int handTotal(ArrayList<Card> hand) {
-        return 0;
+        if (hand.isEmpty())
+            return 0; // empty hand
+        int total = 0;
+        for (int i = 0; i < hand.size(); i++) {
+            total += hand.get(i).value;
+        }
+        if (total >= 10)
+            return total % 10;
+        if (total >= 20)
+            return total % 20;
+        return total; // adds hand together
     }
 
     public boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard) {
-        return true;
+        int total = handTotal(hand);
+        if (total >= 7)
+            return false; // do not draw, stand
+        else if (total == 0 || total == 1 || total == 2)
+            return true; // draw another card
+        else if ((total == 3 || total == 4 || total == 5) && playerCard.value == 0)
+            return true;
+        else if (total == 3 && (playerCard.value != 8 || !playerDrew))
+            return true;
+        else if (total == 4 && (!playerDrew || (playerCard.value >= 2 && playerCard.value <= 7)))
+            return true;
+        else if (total == 5 && (!playerDrew || (playerCard.value >= 4 && playerCard.value <= 7)))
+            return true;
+        else if (total == 6 && (playerCard.value == 6 || playerCard.value == 7))
+            return true;
+        return false;
     }
 
     public boolean evaluatePlayerDraw(ArrayList<Card> hand) {
-        return true;
+        int total = handTotal(hand);
+        if (total <= 5) {
+            playerDrew = true;
+            return true;
+        }
+        return false;
     }
 }
