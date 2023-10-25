893d9d7789f18045c68630bc4d88b35ea4915d13
wavygoten <bossmanzach007@gmail.com>
2023-10-25 01:11:14 -0500

    added card images, round number, spacing layout changes, and more

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import javafx.application.Application;
@@ -10,6 +12,8 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -31,6 +35,13 @@ public class BaccaratGame extends Application {
 		return win;
 
 		// do this bc ration multiplication
+	}
+
+	private String getCardImage(Card card) {
+		String cardValue = Integer.toString(card.value); // convert card integer value to string
+		String cardString = "/" + cardValue + card.suite + ".png"; // concatenate int string to suite string to be used
+																	// as a file path
+		return cardString;
 	}
 
 	public static void main(String[] args) {
@@ -58,6 +69,7 @@ public class BaccaratGame extends Application {
 		bankerBetAmount.setPromptText("$0");
 		bankerBetAmount.setDisable(true);
 		Button playButton = new Button("Play Game");
+		playButton.setDisable(true);
 
 		Region spacing4 = new Region();
 		spacing4.setPrefWidth(40);
@@ -80,6 +92,8 @@ public class BaccaratGame extends Application {
 		Label intWinnings = new Label("$0.0");
 		Label balance = new Label("Balance");
 		Label intBalance = new Label("$" + String.valueOf(this.currentBalance));
+		Label round = new Label("Round");
+		Label roundNumber = new Label(String.valueOf(this.roundNumber));
 		// intBalance.setT
 		Region spacing1 = new Region();
 		spacing1.setPrefHeight(20);
@@ -87,11 +101,60 @@ public class BaccaratGame extends Application {
 		spacing2.setPrefHeight(20);
 		Region spacing3 = new Region();
 		spacing3.setPrefHeight(20);
+		Region sp4 = new Region();
+		sp4.setPrefHeight(20);
 		VBox totalMenu = new VBox(bankerTotal, intBankerTotal, spacing1, playerTotal, intPlayerTotal, spacing2,
-				winnings, intWinnings, spacing3, balance, intBalance);
+				winnings, intWinnings, spacing3, balance, intBalance, sp4, round, roundNumber);
 
-		totalMenu.setAlignment(Pos.CENTER);
-		pane.setLeft(totalMenu);
+		// Card test = new Card("Spades", 13);
+
+		ImageView playerCardOneView = new ImageView();
+		ImageView playerCardTwoView = new ImageView();
+		ImageView playerCardThreeView = new ImageView();
+		ImageView bankerCardOneView = new ImageView();
+		ImageView bankerCardTwoView = new ImageView();
+		ImageView bankerCardThreeView = new ImageView();
+
+		playerCardTwoView.setX(10);
+		playerCardTwoView.setY(10);
+		playerCardTwoView.setFitWidth(75);
+		playerCardTwoView.setPreserveRatio(true);
+		playerCardOneView.setX(10);
+		playerCardOneView.setY(10);
+		playerCardOneView.setFitWidth(75);
+		playerCardOneView.setPreserveRatio(true);
+		playerCardThreeView.setX(10);
+		playerCardThreeView.setY(10);
+		playerCardThreeView.setFitWidth(75);
+		playerCardThreeView.setPreserveRatio(true);
+		bankerCardOneView.setX(10);
+		bankerCardOneView.setY(10);
+		bankerCardOneView.setFitWidth(75);
+		bankerCardOneView.setPreserveRatio(true);
+		bankerCardTwoView.setX(10);
+		bankerCardTwoView.setY(10);
+		bankerCardTwoView.setFitWidth(75);
+		bankerCardTwoView.setPreserveRatio(true);
+		bankerCardThreeView.setX(10);
+		bankerCardThreeView.setY(10);
+		bankerCardThreeView.setFitWidth(75);
+		bankerCardThreeView.setPreserveRatio(true);
+		Region space5 = new Region();
+		space5.setPrefWidth(45);
+		Region space6 = new Region();
+		space6.setPrefWidth(20);
+		Region space7 = new Region();
+		space7.setPrefWidth(20);
+		HBox cards1 = new HBox(playerCardOneView, playerCardTwoView, playerCardThreeView);
+		HBox cards2 = new HBox(bankerCardOneView, bankerCardTwoView, bankerCardThreeView);
+		Label playerCards = new Label("Player");
+		Label bankerCards = new Label("Banker");
+		VBox player = new VBox(playerCards, cards1);
+		VBox banker = new VBox(bankerCards, cards2);
+		HBox centerScreen = new HBox(totalMenu, space6, player, space7, banker);
+		centerScreen.setAlignment(Pos.CENTER);
+		pane.setLeft(centerScreen);
+		// totalMenu.setAlignment(Pos.CENTER);
 		pane.setTop(mb);
 		pane.setBottom(bottomBar);
 		Scene scene = new Scene(pane, 700, 700);
@@ -100,7 +163,7 @@ public class BaccaratGame extends Application {
 		primaryStage.show();
 
 		// button functions below
-		playButton.setOnAction(e -> {
+		playButton.setOnAction(event -> {
 			BaccaratGameLogic logic = new BaccaratGameLogic();
 			String betOn = "";
 			double currentBet = 0;
@@ -127,58 +190,140 @@ public class BaccaratGame extends Application {
 				// this.theDealer.print();
 				intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
 				intPlayerTotal.setText(String.valueOf(logic.handTotal(this.playerHand)));
-				// check draw one
-				// player
-				if (logic.evaluatePlayerDraw(this.playerHand)) {
-					this.playerHand.add(this.theDealer.drawOne());
-					intPlayerTotal.setText(String.valueOf(logic.handTotal(this.playerHand)));
+				// this stuff changes
+				playerCardOneView.setImage(null);
+				playerCardTwoView.setImage(null);
+				playerCardThreeView.setImage(null);
+				bankerCardOneView.setImage(null);
+				bankerCardTwoView.setImage(null);
+				bankerCardThreeView.setImage(null);
+
+				try {
+					InputStream playerCardOneStream = new FileInputStream(
+							System.getProperty("user.dir") + "/src/main/resources/cardImages"
+									+ getCardImage(this.playerHand.get(0)));
+					InputStream playerCardTwoStream = new FileInputStream(
+							System.getProperty("user.dir") + "/src/main/resources/cardImages"
+									+ getCardImage(this.playerHand.get(1)));
+					InputStream bankerCardOneStream = new FileInputStream(
+							System.getProperty("user.dir") + "/src/main/resources/cardImages"
+									+ getCardImage(this.bankerHand.get(0)));
+					InputStream bankerCardTwoStream = new FileInputStream(
+							System.getProperty("user.dir") + "/src/main/resources/cardImages"
+									+ getCardImage(this.bankerHand.get(1)));
+					Image playerCardOne = new Image(playerCardOneStream);
+					Image playerCardTwo = new Image(playerCardTwoStream);
+					Image bankerCardOne = new Image(bankerCardOneStream);
+					Image bankerCardTwo = new Image(bankerCardTwoStream);
+					playerCardOneView.setImage(playerCardOne);
+					playerCardTwoView.setImage(playerCardTwo);
+					bankerCardOneView.setImage(bankerCardOne);
+					bankerCardTwoView.setImage(bankerCardTwo);
+					if (logic.evaluatePlayerDraw(this.playerHand)) {
+						this.playerHand.add(this.theDealer.drawOne());
+						InputStream playerCardThreeStream = new FileInputStream(
+								System.getProperty("user.dir") + "/src/main/resources/cardImages"
+										+ getCardImage(this.playerHand.get(2)));
+						Image cardThree = new Image(playerCardThreeStream);
+						playerCardThreeView.setImage(cardThree);
+						intPlayerTotal.setText(String.valueOf(logic.handTotal(this.playerHand)));
+					}
+
+					// banker
+					if (this.playerHand.size() > 2) {
+						if (logic.evaluateBankerDraw(this.playerHand, this.playerHand.get(2))) {
+							this.bankerHand.add(this.theDealer.drawOne());
+							InputStream bankerCardThreeStream = new FileInputStream(
+									System.getProperty("user.dir") + "/src/main/resources/cardImages"
+											+ getCardImage(this.bankerHand.get(2)));
+							Image bankerCardThree = new Image(bankerCardThreeStream);
+							bankerCardThreeView.setImage(bankerCardThree);
+							intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
+						}
+					} else {
+						if (logic.evaluateBankerDraw(this.playerHand, null)) {
+							this.bankerHand.add(this.theDealer.drawOne());
+							InputStream bankerCardThreeStream = new FileInputStream(
+									System.getProperty("user.dir") + "/src/main/resources/cardImages"
+											+ getCardImage(this.bankerHand.get(2)));
+							Image bankerCardThree = new Image(bankerCardThreeStream);
+							bankerCardThreeView.setImage(bankerCardThree);
+							intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
+						}
+					}
+				} catch (Exception error) {
+					error.printStackTrace();
+					// TODO: handle exception
+				} finally {
+					String whoWon = logic.handTotal(this.bankerHand) > logic.handTotal(this.playerHand) ? "B"
+							: logic.handTotal(this.bankerHand) < logic.handTotal(this.playerHand) ? "P" : "T";
+					System.out.println("who won: " + whoWon + "\nbet on: " + betOn);
+					if (whoWon.equals(betOn) && whoWon.equals("T")) {
+						this.currentBalance += (currentBet * 8);
+						this.totalWinnings += (currentBet * 8);
+					} else if (whoWon.equals(betOn) && whoWon.equals("P")) {
+						this.currentBalance += currentBet;
+						this.totalWinnings += currentBet;
+					} else if (whoWon.equals(betOn) && whoWon.equals("B")) {
+						this.currentBalance += (currentBet * 1.05);
+						this.totalWinnings += (currentBet * 1.05);
+					} else {
+						this.currentBalance -= currentBet;
+					}
+					intBalance.setText("$" + String.valueOf(this.currentBalance));
+					intWinnings.setText("$" + String.valueOf(this.totalWinnings));
+					this.roundNumber++;
+					roundNumber.setText(String.valueOf(this.roundNumber));
+					for (int i = 0; i < this.playerHand.size(); i++) {
+						System.out.println(
+								"Player Hand: " + this.playerHand.get(i).value + " of " + this.playerHand.get(i).suite);
+					}
+					for (int i = 0; i < this.bankerHand.size(); i++) {
+						System.out.println("Banker Hand: " + this.bankerHand.get(i).value + " of "
+								+ this.bankerHand.get(i).suite);
+					}
+					System.out.println("Round: " + this.roundNumber);
 				}
 
-				// banker
-				if (this.playerHand.size() > 2) {
-					if (logic.evaluateBankerDraw(this.playerHand, this.playerHand.get(2))) {
-						this.bankerHand.add(this.theDealer.drawOne());
-						intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
-					}
-				} else {
-					if (logic.evaluateBankerDraw(this.playerHand, null)) {
-						this.bankerHand.add(this.theDealer.drawOne());
-						intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
-					}
-				}
+				// if (logic.evaluatePlayerDraw(this.playerHand)) {
+				// this.playerHand.add(this.theDealer.drawOne());
+				// InputStream playerCardThreeStream = new FileInputStream(
+				// System.getProperty("user.dir") + "/src/main/resources/cardImages"
+				// + getCardImage(this.playerHand.get(2)));
+				// Image cardThree = new Image(playerCardThreeStream);
+				// playerCardThreeView.setImage(cardThree);
+
+				// intPlayerTotal.setText(String.valueOf(logic.handTotal(this.playerHand)));
+				// }
+
+				// // banker
+				// if (this.playerHand.size() > 2) {
+				// if (logic.evaluateBankerDraw(this.playerHand, this.playerHand.get(2))) {
+				// this.bankerHand.add(this.theDealer.drawOne());
+				// intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
+				// }
+				// } else {
+				// if (logic.evaluateBankerDraw(this.playerHand, null)) {
+				// this.bankerHand.add(this.theDealer.drawOne());
+				// intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
+				// }
+				// }
 
 				// calculate who won and either append or dont append winnings to current
 				// balance.
-				String whoWon = logic.handTotal(this.bankerHand) > logic.handTotal(this.playerHand) ? "B"
-						: logic.handTotal(this.bankerHand) < logic.handTotal(this.playerHand) ? "P" : "T";
-				System.out.println("who won: " + whoWon + "\nbet on: " + betOn);
-				if (whoWon.equals(betOn) && whoWon.equals("T")) {
-					this.currentBalance += (currentBet * 8);
-					this.totalWinnings += (currentBet * 8);
-				} else if (whoWon.equals(betOn) && whoWon.equals("P")) {
-					this.currentBalance += currentBet;
-					this.totalWinnings += currentBet;
-				} else if (whoWon.equals(betOn) && whoWon.equals("B")) {
-					this.currentBalance += (currentBet * 1.05);
-					this.totalWinnings += (currentBet * 1.05);
-				} else {
-					this.currentBalance -= currentBet;
-				}
-				intBalance.setText("$" + String.valueOf(this.currentBalance));
-				intWinnings.setText("$" + String.valueOf(this.totalWinnings));
-				this.roundNumber++;
-				for (int i = 0; i < this.playerHand.size(); i++) {
-					System.out.println("Player Hand: " + this.playerHand.get(i).value);
-				}
-				for (int i = 0; i < this.bankerHand.size(); i++) {
-					System.out.println("Banker Hand: " + this.bankerHand.get(i).value);
-
-				}
-				System.out.println("Round: " + this.roundNumber);
 
 			} else {
 				System.out.println("DONT HAVE ENOUGH BREAD U GAMBLING DEGENERATE");
 			}
+		});
+		playerBetAmount.textProperty().addListener((observable, oldVal, newVal) -> {
+			playButton.setDisable(false);
+		});
+		bankerBetAmount.textProperty().addListener((observable, oldVal, newVal) -> {
+			playButton.setDisable(false);
+		});
+		tieBetAmount.textProperty().addListener((observable, oldVal, newVal) -> {
+			playButton.setDisable(false);
 		});
 		playerBet.setOnAction(e -> {
 			playerBetAmount.setPromptText("");
@@ -222,6 +367,7 @@ public class BaccaratGame extends Application {
 			this.roundNumber = 1;
 			intWinnings.setText("$" + String.valueOf(this.totalWinnings));
 			intBalance.setText("$" + String.valueOf(this.currentBalance));
+			roundNumber.setText(String.valueOf(this.roundNumber));
 			intBankerTotal.setText("0");
 			intPlayerTotal.setText("0");
 			bankerBetAmount.setPromptText("$0");
@@ -233,6 +379,13 @@ public class BaccaratGame extends Application {
 			playerBetAmount.setPromptText("$0");
 			playerBetAmount.setText("");
 			playerBetAmount.setDisable(true);
+			playerCardOneView.setImage(null);
+			playerCardTwoView.setImage(null);
+			playerCardThreeView.setImage(null);
+			bankerCardOneView.setImage(null);
+			bankerCardTwoView.setImage(null);
+			bankerCardThreeView.setImage(null);
+			playButton.setDisable(true);
 			// playButton.fire();
 		});
 
