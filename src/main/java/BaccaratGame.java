00bcf2f33a468e7bd97a4739addfe6bf60ce004a
wavygoten <bossmanzach007@gmail.com>
2023-10-24 23:29:00 -0500

    fixed buttons, winnings, balance, textfields, gui logic stuff

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -19,8 +20,8 @@ public class BaccaratGame extends Application {
 	private ArrayList<Card> playerHand = new ArrayList<>();
 	private ArrayList<Card> bankerHand = new ArrayList<>();
 	private BaccaratDealer theDealer = new BaccaratDealer();
-	private double currentBalance = 0;
-	private double currentBet = 0;
+	private int roundNumber = 1;
+	private double currentBalance = 100;
 	private double totalWinnings = 0;
 
 	public double evaluateWinnings() {
@@ -44,12 +45,18 @@ public class BaccaratGame extends Application {
 		primaryStage.setTitle("Baccarat Game");
 		BorderPane pane = new BorderPane();
 
-		TextField playerBet = new TextField("Player Bet");
-		TextField playerBetAmount = new TextField("$0");
-		TextField tieBet = new TextField("Tie Bet");
-		TextField tieBetAmount = new TextField("$0");
-		TextField bankerBet = new TextField("Banker Bet");
-		TextField bankerBetAmount = new TextField("$0");
+		Button playerBet = new Button("Player Bet");
+		TextField playerBetAmount = new TextField("");
+		playerBetAmount.setPromptText("$0");
+		playerBetAmount.setDisable(true);
+		Button tieBet = new Button("Tie Bet");
+		TextField tieBetAmount = new TextField("");
+		tieBetAmount.setPromptText("$0");
+		tieBetAmount.setDisable(true);
+		Button bankerBet = new Button("Banker Bet");
+		TextField bankerBetAmount = new TextField("");
+		bankerBetAmount.setPromptText("$0");
+		bankerBetAmount.setDisable(true);
 		Button playButton = new Button("Play Game");
 
 		Region spacing4 = new Region();
@@ -65,22 +72,15 @@ public class BaccaratGame extends Application {
 		MenuItem re = new MenuItem("Fresh Start");
 		mb.getMenus().add(options);
 		options.getItems().addAll(exit, re);
-		TextField bankerTotal = new TextField("Banker total");
-		bankerTotal.setEditable(false);
-		TextField intBankerTotal = new TextField("0");
-		intBankerTotal.setEditable(false);
-		TextField playerTotal = new TextField("Player total");
-		playerTotal.setEditable(false);
-		TextField intPlayerTotal = new TextField("0");
-		intPlayerTotal.setEditable(false);
-		TextField winnings = new TextField("Winnings");
-		winnings.setEditable(false);
-		TextField intWinnings = new TextField("$0");
-		intWinnings.setEditable(false);
-		TextField balance = new TextField("Balance");
-		balance.setEditable(false);
-		TextField intBalance = new TextField("$0");
-		intBalance.setEditable(false);
+		Label bankerTotal = new Label("Banker total");
+		Label intBankerTotal = new Label("0");
+		Label playerTotal = new Label("Player total");
+		Label intPlayerTotal = new Label("0");
+		Label winnings = new Label("Winnings");
+		Label intWinnings = new Label("$0.0");
+		Label balance = new Label("Balance");
+		Label intBalance = new Label("$" + String.valueOf(this.currentBalance));
+		// intBalance.setT
 		Region spacing1 = new Region();
 		spacing1.setPrefHeight(20);
 		Region spacing2 = new Region();
@@ -93,13 +93,7 @@ public class BaccaratGame extends Application {
 		totalMenu.setAlignment(Pos.CENTER);
 		pane.setLeft(totalMenu);
 		pane.setTop(mb);
-		// pane.setRight(options);
 		pane.setBottom(bottomBar);
-		bankerTotal.getStyleClass().add("transparent_border");
-		playerTotal.getStyleClass().add("transparent_border");
-		winnings.getStyleClass().add("transparent_border");
-		balance.getStyleClass().add("transparent_border");
-
 		Scene scene = new Scene(pane, 700, 700);
 		scene.getStylesheets().add("styles.css");
 		primaryStage.setScene(scene);
@@ -108,18 +102,30 @@ public class BaccaratGame extends Application {
 		// button functions below
 		playButton.setOnAction(e -> {
 			BaccaratGameLogic logic = new BaccaratGameLogic();
-
+			String betOn = "";
+			double currentBet = 0;
+			if (playerBetAmount.getText() == null && tieBetAmount.getText() == null) {
+				currentBet = Integer.valueOf(bankerBetAmount.getText());
+				betOn = "B";
+			} else if (playerBetAmount.getText() == null && bankerBetAmount.getText() == null) {
+				currentBet = Integer.valueOf(tieBetAmount.getText());
+				betOn = "T";
+			} else {
+				currentBet = Integer.valueOf(playerBetAmount.getText());
+				betOn = "P";
+			}
+			// System.out.println(betOn);
+			// this.currentBet = playerBetAmount.getText() != null ?
+			// Integer.valueOf(playerBetAmount.getText())
+			// : tieBetAmount.getText() != null ? Integer.valueOf(tieBetAmount.getText())
+			// : bankerBetAmount.getText() != null ?
+			// Integer.valueOf(bankerBetAmount.getText()) : 0;
 			this.theDealer.shuffleDeck();
-			// fix error here
-			this.playerHand = this.theDealer.dealHand(); // 4
-
-			this.bankerHand = this.theDealer.dealHand(); // 3
+			this.playerHand = this.theDealer.dealHand();
+			this.bankerHand = this.theDealer.dealHand();
 			// this.theDealer.print();
-			// for (int i = 0; i < this.playerHand.size(); i++) {
-			// System.out.println(this.playerHand.get(i).value);
-			// }
-			intBankerTotal.setText(String.valueOf(logic.handTotal(this.playerHand)));
-			intPlayerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
+			intBankerTotal.setText(String.valueOf(logic.handTotal(this.bankerHand)));
+			intPlayerTotal.setText(String.valueOf(logic.handTotal(this.playerHand)));
 			// check draw one
 			// player
 			if (logic.evaluatePlayerDraw(this.playerHand)) {
@@ -140,56 +146,79 @@ public class BaccaratGame extends Application {
 				}
 			}
 
+			// calculate who won and either append or dont append winnings to current
+			// balance.
+			String whoWon = logic.handTotal(this.bankerHand) > logic.handTotal(this.playerHand) ? "B"
+					: logic.handTotal(this.bankerHand) < logic.handTotal(this.playerHand) ? "P" : "T";
+			System.out.println("who won: " + whoWon + "\nbet on: " + betOn);
+			if (whoWon.equals(betOn) && whoWon.equals("T")) {
+				this.currentBalance += (currentBet * 8);
+				this.totalWinnings += (currentBet * 8);
+			} else if (whoWon.equals(betOn) && whoWon.equals("P")) {
+				this.currentBalance += currentBet;
+				this.totalWinnings += currentBet;
+			} else if (whoWon.equals(betOn) && whoWon.equals("B")) {
+				this.currentBalance += (currentBet * 1.05);
+				this.totalWinnings += (currentBet * 1.05);
+			} else {
+				this.currentBalance -= currentBet;
+			}
+			intBalance.setText("$" + String.valueOf(this.currentBalance));
+			intWinnings.setText("$" + String.valueOf(this.totalWinnings));
+			this.roundNumber++;
+
 			for (int i = 0; i < this.playerHand.size(); i++) {
 				System.out.println("Player Hand: " + this.playerHand.get(i).value);
 			}
 			for (int i = 0; i < this.bankerHand.size(); i++) {
 				System.out.println("Banker Hand: " + this.bankerHand.get(i).value);
 			}
+			System.out.println("Round: " + this.roundNumber);
 
 		});
-
+		playerBet.setOnAction(e -> {
+			playerBetAmount.setPromptText("");
+			playerBetAmount.setDisable(false);
+			tieBetAmount.setPromptText("$0");
+			tieBetAmount.setDisable(true);
+			tieBetAmount.setText(null);
+			bankerBetAmount.setPromptText("$0");
+			bankerBetAmount.setDisable(true);
+			bankerBetAmount.setText(null);
+		});
+		tieBet.setOnAction(e -> {
+			playerBetAmount.setPromptText("$0");
+			playerBetAmount.setDisable(true);
+			playerBetAmount.setText(null);
+			tieBetAmount.setPromptText("");
+			tieBetAmount.setDisable(false);
+			bankerBetAmount.setPromptText("$0");
+			bankerBetAmount.setDisable(true);
+			bankerBetAmount.setText(null);
+		});
+		bankerBet.setOnAction(e -> {
+			playerBetAmount.setPromptText("$0");
+			playerBetAmount.setDisable(true);
+			playerBetAmount.setText(null);
+			tieBetAmount.setPromptText("$0");
+			tieBetAmount.setDisable(true);
+			tieBetAmount.setText(null);
+			bankerBetAmount.setPromptText("");
+			bankerBetAmount.setDisable(false);
+		});
 		exit.setOnAction(e -> {
 			Platform.exit();
 			System.exit(0);
 		});
 		re.setOnAction(e -> {
 			this.currentBalance = 100;
-			this.currentBet = 0;
 			this.totalWinnings = 0;
 			this.bankerHand.clear();
 			this.playerHand.clear();
-			playButton.fire();
+			this.roundNumber = 1;
+			// playButton.fire();
 		});
 
 	}
-
-	private void play() {
-
-		// bankTextField.setText(String.valueOf(bankerTotal));
-		// playerTextField.setText(String.valueOf(playerTotal));
-
-	}
-
-	private void freshRestart() {
-
-		this.currentBalance = 100;
-		this.theDealer.generateDeck();
-		this.theDealer.shuffleDeck();
-		this.currentBet = 0;
-		this.totalWinnings = 0;
-		this.bankerHand.clear();
-		this.playerHand.clear();
-		// this.play();
-		// do new play game
-
-	}
-	// private BorderPane setupWelcomePage() {
-
-	// BorderPane pane = new BorderPane();
-	// pane.getStyleClass().add("pane");
-
-	// return pane;
-	// }
 
 }
