7842fc7fe0f4e03c2ee4761bf8d841f3b31d6ddc
doggytoaster <75501612+doggytoaster@users.noreply.github.com>
2023-10-29 19:24:31 -0500

    Update Card.java

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,3 +1,14 @@
+/**************************
+ *Program 2: Baccarat game - Card.java
+ This file contains the card container which contains the cards suite and cards value, it is essential to baccarat since
+ it is what builds the deck of 52 cards that the game is played with
+
+ Course: CS 342, Fall 2023.
+ System: Windows using Intellij
+ Starter Code Author: Mark Hallenbeck
+ Student Authors: Adrian Lopez, Ihsaan Bijapuri
+ *
+ *************************/
 public class Card {
     public String suite;
     public int value;
